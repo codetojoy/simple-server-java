ad262aa4c782c71c729f9fbda86c9821cf409652
Michael Easter <codetojoy@gmail.com>
2025-04-25 19:51:58 -0300

    meta max-size param

--- run_client_page.sh
+++ run_client_page.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -X GET "http://localhost:8080/account/page?pageNum=0&pageSize=20" | jq > page0.json
+curl -X GET "http://localhost:8080/account/page?page_num=0&page_size=20&meta_max_size=50" | jq > page0.json
 
-curl -X GET "http://localhost:8080/account/page?pageNum=1&pageSize=20" | jq > page1.json
+curl -X GET "http://localhost:8080/account/page?page_num=1&page_size=20&meta_max_size=50" | jq > page1.json
 
-curl -X GET "http://localhost:8080/account/page?pageNum=2&pageSize=20" | jq > page2.json
+curl -X GET "http://localhost:8080/account/page?page_num=2&page_size=20&meta_max_size=50" | jq > page2.json
 
--- src/main/java/net/codetojoy/simple/service/AccountByPageService.java
+++ src/main/java/net/codetojoy/simple/service/AccountByPageService.java
@@ -20,27 +20,39 @@ import net.codetojoy.simple.entity.Result;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AccountByPageService {
 
+    private String now() {
+        return new Date().toString();
+    }
+
+    private void log(String msg) {
+        System.out.println("TRACER " + now() + " : " + msg);
+    }
+
     @GetMapping("/page")
     public ResponseEntity<List<Result>> fetchByPage(
-                                    @RequestParam(name="pageNum") int pageNum,
-                                    @RequestParam(name="pageSize") int pageSize,
-                                    @RequestParam(name="delay_in_seconds", required=false) Integer delayInSeconds
+                                    @RequestParam(name="page_num") int pageNum,
+                                    @RequestParam(name="page_size") int pageSize,
+                                    @RequestParam(name="meta_max_size") int maxSize,
+                                    @RequestParam(name="meta_delay_in_seconds", required=false) Integer delayInSeconds
                                     ) throws InputException {
 
         if (delayInSeconds != null) {
             try {
-                System.out.println("TRACER pathological delay: " + delayInSeconds + " sec");
+                log("pathological delay: " + delayInSeconds + " sec");
                 Thread.sleep(delayInSeconds * 1000);
             } catch (Exception ex) {
             }
         }
 
         int start = (pageNum * pageSize) + 1;
-        String now = new Date().toString();
-        String prefix = "TRACER " + now + " ";
-        System.out.println(prefix + "pageNum: " + pageNum 
-                                  + " pageSize: " + pageSize
-                                  + " start: " + start);
+
+        // guard
+        if (start >= maxSize) {
+            log("empty! start: " + start + " maxSize: " + maxSize);
+            return new ResponseEntity<List<Result>>(List.of(), HttpStatus.OK);
+        }
+
+        log("pageNum: " + pageNum + " pageSize: " + pageSize + " start: " + start);
 
         List<Result> results = new ArrayList<>();
         boolean isDone = false;
@@ -50,7 +62,7 @@ public class AccountByPageService {
             int id = start + count; 
             boolean enrolled = true;
             String name = "name-" + id;
-            String message = now + " OK [" + id + "]";
+            String message = now() + " OK [" + id + "]";
 
             Result result = new Result();
             result.setId(id);
@@ -61,7 +73,7 @@ public class AccountByPageService {
             results.add(result);
     
             count++;
-            isDone = (count >= pageSize);
+            isDone = (count >= pageSize) || (id == maxSize);
         }
 
         return new ResponseEntity<List<Result>>(results, HttpStatus.OK);
